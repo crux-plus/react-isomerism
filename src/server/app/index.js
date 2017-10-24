5cf39b2958c8187892158b91611d4d87b80d82c9
crux wild <18210841568@163.com>
2017-10-24 18:27:05 +0800

    add

---
+++ src/server/app/index.js
@@ -0,0 +1,6 @@
+// Expressive middleware for node.js using ES2017 async functions.k
+import Koa from 'koa';
+
+const app = new Koa();
+
+export default app;
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,8 @@
 // require the rest of the app that needs to be transpiled after the hook
 require('babel-core/register');
 
+import http from 'http';
+
 import app from 'server/app';
+
+http.createServer(app.callback()).listen(3000);
